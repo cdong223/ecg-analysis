b600c98c12b8d2b1f479c1e68f4a80c3fd719c4c
Claire Dong <claire.dong@duke.edu>
2019-10-10 17:32:30 -0400

    parse_add() to convert time/voltage values from csv file into floats accounting for errors/nan

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -3,7 +3,18 @@ import math
 
 
 def parse_add(t, v, time, voltage):
-    return
+    try:  # if t is non-numeric or empty then skip
+        t = float(t)
+    except ValueError:
+        return
+    try:  # if v is non-numeric or empty then skip
+        v = float(v)
+    except ValueError:
+        return
+    if math.isnan(t) is True or math.isnan(v) is True:  # if NaN
+        return
+    time.append(t)
+    voltage.append(v)
 
 
 def import_data(filename):
@@ -14,17 +25,13 @@ def import_data(filename):
         for row in reader:
             t = row[0]
             v = row[1]
-            # parse_add(t, v, time, voltage)
-            time.append(t)
-            voltage.append(v)
+            parse_add(t, v, time, voltage)
     return time, voltage
 
 
 def main():
     filename = input("Enter filename: ")
     time, voltage = import_data(filename)
-    print(time)
-    print(voltage)
 
 
 if __name__ == '__main__':
