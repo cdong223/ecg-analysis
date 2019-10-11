7a780dcd41b0d31ff62795e6f846c545f2fbab7a
Claire Dong <claire.dong@duke.edu>
2019-10-10 20:33:48 -0400

    added logging functions for missing/bad data, NaN, and voltage values outside normal range

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -1,17 +1,28 @@
 import csv
 import math
+import logging
+
+
+def check_range(voltage):
+    for v in voltage:
+        if v < -300 or v > 300:
+            logging.warning("Voltage contains values outside the normal range")
+            return
 
 
 def parse_add(t, v, time, voltage):
     try:  # if t is non-numeric or empty then skip
         t = float(t)
     except ValueError:
+        logging.error("'{}' cannot be cast as a float.".format(t))
         return
     try:  # if v is non-numeric or empty then skip
         v = float(v)
     except ValueError:
+        logging.error("'{}' cannot be cast as a float.".format(v))
         return
-    if math.isnan(t) is True or math.isnan(v) is True:  # if NaN
+    if math.isnan(t) is True or math.isnan(v) is True:  # if NaN then skip
+        logging.error("Value is NaN.")
         return
     time.append(t)
     voltage.append(v)
@@ -26,10 +37,13 @@ def import_data(filename):
             t = row[0]
             v = row[1]
             parse_add(t, v, time, voltage)
+    check_range(voltage)
     return time, voltage
 
 
 def main():
+    logging.basicConfig(filename="sequence.log", level=logging.DEBUG,
+                        filemode="w")
     filename = input("Enter filename: ")
     time, voltage = import_data(filename)
 
